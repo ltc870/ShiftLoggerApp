6e4853f10adc01b0e4aaa548fb2ed9a94e1d4882
Lawrence C. <lawrencecaudle870@gmail.com>
2025-07-06 21:56:18 -0400

    changed format of the ShiftDto record

--- ShiftLoggerApi/ShiftLoggerApi/Dtos/ShiftDto.cs
+++ ShiftLoggerApi/ShiftLoggerApi/Dtos/ShiftDto.cs
@@ -1,10 +1,6 @@
 namespace ShiftLoggerApi.Dtos;
 
-public record ShiftDto(
-    int ShiftId,
-    DateTime ShiftStart,
-    DateTime ShiftEnd,
-    int EmployeeId)
+public record ShiftDto(int ShiftId, DateTime ShiftStart, DateTime ShiftEnd, int EmployeeId)
 {
     public TimeSpan ShiftDuration => ShiftEnd - ShiftStart;
 }

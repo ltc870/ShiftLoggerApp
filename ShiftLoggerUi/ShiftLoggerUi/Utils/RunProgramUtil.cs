88024a4371671f98b97bad1c7b9a26c361fa8b58
Lawrence C. <lawrencecaudle870@gmail.com>
2025-07-10 21:55:26 -0400

    added method to manage employees

--- ShiftLoggerUi/ShiftLoggerUi/Utils/RunProgramUtil.cs
+++ ShiftLoggerUi/ShiftLoggerUi/Utils/RunProgramUtil.cs
@@ -37,11 +37,67 @@ public class RunProgramUtil
                     break;
                 case "1":
                     Console.WriteLine("Manage Employees selected.");
-                    // Call method to manage employees
+                    ManageEmployees();
                     break;
                 case "2":
                     Console.WriteLine("Manage Shifts selected.");
                     // Call method to manage shifts
+                    break;
+                default:
+                    Console.WriteLine("Invalid input, try again.");
+                    Console.WriteLine("Press any key to continue...");
+                    Console.ReadKey();
+                    break;
+            }
+        }
+    }
+
+    private static void ManageEmployees()
+    {
+        Console.Clear();
+        bool employeeManagementRunning = true;
+        
+        
+        while (employeeManagementRunning)
+        {
+            Console.Clear();
+            Console.WriteLine("Employee Management Menu:");
+            Console.WriteLine("<---------------------------------------------------------->\n");
+            Console.WriteLine("Type 0 to Return to Main Menu.");
+            Console.WriteLine("Type 1 to View All Employees");
+            Console.WriteLine("Type 2 To View An Employee");
+            Console.WriteLine("Type 3 To Add An Employee");
+            Console.WriteLine("Type 4 To Update An Employee");
+            Console.WriteLine("Type 5 To Delete An Employee");
+
+            string userInput = Console.ReadLine();
+
+            switch (userInput)
+            {
+                case "0":
+                    Console.Clear();
+                    employeeManagementRunning = false;
+                    UserOptions();
+                    break;
+                case "1":
+                    Console.WriteLine("View All Employees selected.");
+                    // Call method to view all employees
+                    break;
+                case "2":
+                    Console.WriteLine("View An Employee selected.");
+                    // Call method to view an employee
+                    break;
+                case "3":
+                    Console.WriteLine("Add An Employee selected.");
+                    // Call method to add an employee
+                    break;
+                case "4":
+                    Console.WriteLine("Update An Employee selected.");
+                    // Call method to update an employee
+                    break;
+                case "5":
+                    Console.WriteLine("Delete An Employee selected.");
+                    // Call method to delete an employee
                     break;
                 default:
                     Console.WriteLine("Invalid input, try again.");
